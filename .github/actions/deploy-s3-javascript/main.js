7d4327de4b4ae76632cf5f206b5ac8e11cc70f87
Dhanika Kumarasiri <dhanikaanupama2000@gmail.com>
2024-08-18 02:26:05 +0530

    added custom deploy action with output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,10 +7,11 @@ function run() {
     const bucketRegion = core.getInput('bucket-region', {required: true});
     const distFolder = core.getInput('dist-folder', {required: true});
 
-    const s3Uri = `s3://${bucket}`;
+    const s3Uri = `s3://${bucket}`; 
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom j avascript action');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
